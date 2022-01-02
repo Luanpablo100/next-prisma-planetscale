003da86a333036090ceacf81983708cdfb6238f8
Luan Oliveira <lpablo611@gmail.com>
2022-01-02 19:29:11 -0400

    no background image test

--- components/Container.js
+++ components/Container.js
@@ -10,15 +10,15 @@ export default function Container({children}) {
                 <title>Planetscale App</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className="flex flex-col items-center justify-center min-h-screen font-bold text-white">
+            <div className="flex flex-col items-center justify-center min-h-screen font-bold text-white bg-gray-900">
                 <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center z-40">
                     {children}
                 </main>
-            <img src="/img/2xl-moon.svg" className="w-screen h-screen absolute z-0"></img>
+            {/* <img src="/img/2xl-moon.svg" className="w-screen h-screen absolute z-0"></img>
             <img src="/img/lg-moon.svg" className="w-screen h-screen absolute z-0 xl:hidden"></img>
             <img src="/img/md-moon.svg" className="w-screen h-screen absolute z-0 lg:hidden"></img>
             <img src="/img/sm-moon.svg" className="w-screen h-screen absolute z-0 md:hidden"></img>
-            <img src="/img/xsm-moon.svg" className="w-screen h-screen absolute z-0 sm:hidden"></img>
+            <img src="/img/xsm-moon.svg" className="w-screen h-screen absolute z-0 sm:hidden"></img> */}
             </div>
         </>
     )
